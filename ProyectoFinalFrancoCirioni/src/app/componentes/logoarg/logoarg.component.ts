e92a58c5992f8c1a3e0516344fee0d6003ffa719
francocirioni <franco.cirioni.cssm@gmail.com>
2022-07-29 15:13:50 -0300

    banner

---
+++ ProyectoFinalFrancoCirioni/src/app/componentes/logoarg/logoarg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logoarg',
+  templateUrl: './logoarg.component.html',
+  styleUrls: ['./logoarg.component.css']
+})
+export class LogoargComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
